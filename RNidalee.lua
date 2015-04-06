78ada34995a0fb59b0c1fca1def4357806a39ceb
Radi321 <radimpc@yahoo.com>
2015-04-06 16:34:28 +0200

    RNidalee
    hehe

---
+++ RNidalee.lua
@@ -0,0 +1,24 @@
+--[[by RadiChuj
+without flame guys pls]]--
+
+if myHero.charName ~= Nidalee then return end
+
+function OnLoad()
+	FapFap()
+end
+
+function FapFap()
+	Radi()
+end
+
+function Radi()
+	YourMum()
+end
+
+function YourMum()
+	Awesome()
+end
+
+function Awesome()
+	PrintChat("Nidalee is ready to rape enemy!")
+end
